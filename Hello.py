d588c89c89458c4fd2afd989e8efd467a25184bb
Kirthivasan <kirthivasanp@gmail.com>
2024-04-20 14:21:34 -0400

    modified requirements.txt

--- Hello.py
+++ Hello.py
@@ -11,6 +11,7 @@ def create_server_connection():
     try:
         connection = mysql.connector.connect(
             host="localhost",
+            port=3306,
             user="root",
             password="python19",
             database="optiflow"
